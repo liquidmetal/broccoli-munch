9b27279def49803d725bd76121746d9be3f7f26d
Utkarsh Sinha <sinha.utkarsh1990@gmail.com>
2016-03-20 21:04:23 -0400

    Crawler now talks to rabbitmq

--- src/crawler/crawler.go
+++ src/crawler/crawler.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 	"munch/config"
 	"munch/database"
+	"munch/messaging"
 	"munch/stories"
-	"os"
-	"strconv"
 )
 
 // Worker process for fetching data from a given source
@@ -18,32 +17,27 @@ func main() {
 	// Step 4: Put the data into a DB somewhere
 	// Step 5: Repeat
 
-	if len(os.Args) != 2 {
-		fmt.Printf("Usage: %s <id>\n", os.Args[0])
-		return
-	}
-
 	cfg := config.New()
-	sourceid, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Printf("There was an error converting source ID to integer (%s)\n", sourceid)
-		return
+	broker := messaging.New(cfg)
+
+	for true {
+		fmt.Printf("Waiting for crawl request...\n")
+		sourceid := broker.DequeueCrawl()
+		db := database.New(cfg)
+		source := db.FetchSource(sourceid)
+		fmt.Printf("Fetching %s...\n", source.GetName())
+
+		articles := source.FetchNewData()
+
+		if articles != nil {
+			s := source.GenerateStories(articles)
+			persistStories(s, db)
+		} else {
+			fmt.Println("Yo something happened here\n")
+		}
+		db.PersistSource(source)
+		db.Close()
 	}
-
-	db := database.New(cfg)
-	source := db.FetchSource(sourceid)
-	fmt.Printf("Fetching %s...\n", source.GetName())
-
-	//f := sources.NewSourceRss("Techcrunch", "http://feeds.feedburner.com/TechCrunch/?fmt=xml", 0)
-	//f := sources.NewSourceRss("Utkarsh Sinha", "http://utkarshsinha.com/index.xml", 0)
-	articles := source.FetchNewData()
-
-	if articles != nil {
-		s := source.GenerateStories(articles)
-		persistStories(s, db)
-	}
-	db.PersistSource(source)
-	db.Close()
 }
 
 func persistStories(stories []stories.Story, db *database.Db) {
